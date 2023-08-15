6aa2c4546337195243a4c9f4af81b53509fd6e76
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-08-16 01:21:25 +0530

    Updated App.js for keeratBooks route using AutoPilot

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+const keeratBooksRoute = require('./routes/keeratBooks');
+app.use('/keeratBooks', keeratBooksRoute);
 
 //Please don't delete this health API
 app.use('/api/health', (req, res) => {
